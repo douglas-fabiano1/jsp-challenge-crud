a902a5e5cfc6d124e9b080633013901dfdd4a51d
Douglas Fabiano <contatodouglasfabiano1@gmail.com>
2023-11-29 15:13:19 -0300

    Client Entity, Properties, BD seeding

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Renato Gonçalves Nunes', '356.843.225-86', 3000.0, '2000-08-23', 3),
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Sabrina Oliveira Bernades', '735.858.033-16', 4000.0, '1980-02-18', 2),
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Felipe Raposo Silva', '438.243.625-89', 5000.0, '1998-07-27', 1),
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Bruno Mendonça Escobar', '852.146.285-32', 2000.0, '2001-05-01', 4),
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Gislaine Fagundes Medeiros ', '246.244.201-25', 1500.0, '2005-01-26', 0),
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Leonardo Fernandes', '166.078.782-41', 5000.0, '1982-03-17', 1),
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Marcio Labotan Quota', '562.482.510-15', 2000.0, '2003-02-13', 2),
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Vivian Gadu Naldes', '349.218.325-48', 4000.0, '1994-03-28', 3),
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Patricia Cavani Frates', '656.261.527-96', 5000.0, '1970-05-22', 5),
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Pedro Esteban Zadro', '246.381.136-89', 4500.0, '1975-07-24', 0);
