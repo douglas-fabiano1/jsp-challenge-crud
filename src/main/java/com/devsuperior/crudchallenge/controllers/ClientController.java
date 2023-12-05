7a8f1db39ddc729f5316db525c3de7a60fe84eff
Douglas Fabiano <contatodouglasfabiano1@gmail.com>
2023-12-05 17:42:07 -0300

    GET Clients paged

--- src/main/java/com/devsuperior/crudchallenge/controllers/ClientController.java
+++ src/main/java/com/devsuperior/crudchallenge/controllers/ClientController.java
@@ -3,6 +3,8 @@ package com.devsuperior.crudchallenge.controllers;
 import com.devsuperior.crudchallenge.dto.ClientDTO;
 import com.devsuperior.crudchallenge.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +18,11 @@ public class ClientController {
 
     @GetMapping(value = "/{id}")
     public ClientDTO findById(@PathVariable Long id) {
-        ClientDTO dto = service.findById(id);
-        return dto;
+        return service.findById(id);
+    }
+
+    @GetMapping
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
     }
 }
--- src/main/java/com/devsuperior/crudchallenge/services/ClientService.java
+++ src/main/java/com/devsuperior/crudchallenge/services/ClientService.java
@@ -4,10 +4,10 @@ import com.devsuperior.crudchallenge.dto.ClientDTO;
 import com.devsuperior.crudchallenge.entities.Client;
 import com.devsuperior.crudchallenge.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -16,9 +16,13 @@ public class ClientService {
 
     @Transactional(readOnly = true)
     public ClientDTO findById(Long id) {
-        Optional<Client> result = repository.findById(id);
-        Client client = result.get();
-        ClientDTO dto = new ClientDTO(client);
-        return dto;
+        Client client = repository.findById(id).get();
+        return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
     }
 }
