960b5c28e7898c0ee68d3a7fc2fae15b0baf0a70
Douglas Fabiano <contatodouglasfabiano1@gmail.com>
2023-12-08 12:11:30 -0300

    Bean Validation

--- src/main/java/com/devsuperior/crudchallenge/dto/ClientDTO.java
+++ src/main/java/com/devsuperior/crudchallenge/dto/ClientDTO.java
@@ -1,15 +1,19 @@
 package com.devsuperior.crudchallenge.dto;
 
 import com.devsuperior.crudchallenge.entities.Client;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
 
 import java.time.LocalDate;
 
 public class ClientDTO {
 
     private Long id;
+    @NotBlank(message = "Campo não pode ser vazio")
     private String name;
     private String cpf;
     private Double income;
+    @PastOrPresent(message = "Não pode ser data futura")
     private LocalDate birthDate;
     private Integer children;
 
